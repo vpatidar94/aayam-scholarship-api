69beba850f840b44addf028b9d47aa0401e351a2
Jitendra Yadav <jitendraryadav45@gmail.com>
2024-04-17 19:49:24 +0530

    changes related to aset form[signupOtp api and signup api] in user-controller.js

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -30,6 +30,46 @@ const sendOTPMessage = async (req, res) => {
     }
 };
 
+// const signupOTP = async (req, res) => {                                 //signOtp for jeet scholarship test
+//     const data = req.body;
+//     const { mobileNo, testCenterId, mode } = data;
+//     //console.log("mobileNo", mobileNo)
+//     try {
+//         const existingUser = await User.findOne({ mobileNo: mobileNo })
+//         if (existingUser) {
+//             return res.status(403).json({ code: 403, status_code: 'error', message: 'user already exist' })
+//         }
+//         // Check if the mode is "online" (no need to select a test center)
+//         if (mode === 'online') {
+//             await sendOTPMessage(req, res);
+//             return;
+//         }
+
+//         // Check if there are available seats in any test center
+//         const centers = await Center.find();
+//         const availableCenter = centers.find(center => center.capacity > 0);
+
+//         if (!availableCenter) {
+//             // If no seats are available in any center, send a message to contact a specified mobile number
+//             return res.status(400).json({ code: 400, status_code: 'error', message: 'No seats available in any test center. Contact XXXXXXXXXX for more information.' });
+//         }
+//         // Check if the chosen test center has available capacity
+//         const center = await Center.findOne({ centerId: testCenterId });
+//         //console.log("centerrrrr", center)
+//         if (center && center.capacity > 0) {
+//             await sendOTPMessage(req, res);
+//         } else {
+//             // If the center is full or not found, prevent user registration and return an error
+//             return res.status(400).json({ data: center, code: 400, status_code: 'error', message: 'Chosen test center is full or not found' });
+//         }
+//     }
+//     catch (error) {
+//         return res.status(500).json({ code: 500, status_code: "error" })
+//     }
+// }
+
+
+// signupOtp for aset:
 const signupOTP = async (req, res) => {
     const data = req.body;
     const { mobileNo, testCenterId, mode } = data;
@@ -44,29 +84,113 @@ const signupOTP = async (req, res) => {
             await sendOTPMessage(req, res);
             return;
         }
+        if (mode === 'offline') {
+            await sendOTPMessage(req, res);
+            return;
+        }
 
         // Check if there are available seats in any test center
-        const centers = await Center.find();
-        const availableCenter = centers.find(center => center.capacity > 0);
+        // const centers = await Center.find();
+        // const availableCenter = centers.find(center => center.capacity > 0);
 
-        if (!availableCenter) {
-            // If no seats are available in any center, send a message to contact a specified mobile number
-            return res.status(400).json({ code: 400, status_code: 'error', message: 'No seats available in any test center. Contact XXXXXXXXXX for more information.' });
-        }
-        // Check if the chosen test center has available capacity
-        const center = await Center.findOne({ centerId: testCenterId });
-        //console.log("centerrrrr", center)
-        if (center && center.capacity > 0) {
-            await sendOTPMessage(req, res);
-        } else {
-            // If the center is full or not found, prevent user registration and return an error
-            return res.status(400).json({ data: center, code: 400, status_code: 'error', message: 'Chosen test center is full or not found' });
-        }
+        // if (!availableCenter) {
+        //     // If no seats are available in any center, send a message to contact a specified mobile number
+        //     return res.status(400).json({ code: 400, status_code: 'error', message: 'No seats available in any test center. Contact XXXXXXXXXX for more information.' });
+        // }
+        // // Check if the chosen test center has available capacity
+        // const center = await Center.findOne({ centerId: testCenterId });
+        // //console.log("centerrrrr", center)
+        // if (center && center.capacity > 0) {
+        //     await sendOTPMessage(req, res);
+        // } else {
+        //     // If the center is full or not found, prevent user registration and return an error
+        //     return res.status(400).json({ data: center, code: 400, status_code: 'error', message: 'Chosen test center is full or not found' });
+        // }
     }
     catch (error) {
         return res.status(500).json({ code: 500, status_code: "error" })
     }
 }
+
+//signup api for jeet scholarship
+// const signup = async (req, res) => {
+//     //Existing user check
+//     const { name, mobileNo, dob, fatherName, fatherMobileNo, stream, schoolName, city, testDate, mode, testCenterId } = req.body;
+//     try {
+//         const existingUser = await User.findOne({ mobileNo: mobileNo })
+//         if (existingUser) {
+//             return res.status(400).json({ code: 400, status_code: 'error', message: 'user already exist' })
+//         }
+
+//         // Check if the mode is "online" (no need to select a test center)
+//         if (mode === 'online') {
+//             const user = await User.create({
+//                 mobileNo: mobileNo,
+//                 name: name,
+//                 dob: dob,
+//                 fatherName: fatherName,
+//                 fatherMobileNo: fatherMobileNo,
+//                 stream: stream,
+//                 schoolName: schoolName,
+//                 city: city,
+//                 testDate: testDate,
+//                 mode: mode,
+//             });
+
+//             await sendTestInfo(mobileNo, mode, testDate);
+//             return res.status(201).json({ data: { user, isNew: false }, code: 200, status_code: "success", message: "User updated successfully." });
+//         }
+
+//         // Check if there are available seats in any test center
+//         const centers = await Center.find();
+//         const availableCenter = centers.find(center => center.capacity > 0);
+
+//         if (!availableCenter) {
+//             // If no seats are available in any center, send a message to contact a specified mobile number
+//             return res.status(400).json({ code: 400, status_code: 'error', message: 'No seats available in any test center. Contact XXXXXXXXXX for more information.' });
+//         }
+
+//         // Check if the chosen test center has available capacity
+//         const center = await Center.findOne({ centerId: testCenterId });
+
+//         if (center && center.capacity > 0) {
+//             // //Hashed password
+//             // const hashedPassword = await bcrypt.hash(password, 10);
+//             //user create
+//             const user = await User.create({
+//                 mobileNo: mobileNo,
+//                 name: name,
+//                 dob: dob,
+//                 fatherName: fatherName,
+//                 fatherMobileNo: fatherMobileNo,
+//                 stream: stream,
+//                 schoolName: schoolName,
+//                 city: city,
+//                 testDate: testDate,
+//                 mode: mode,
+//                 testCenter: center._id
+
+//             })
+//             // Update the center's capacity
+//             center.capacity -= 1;
+//             await center.save();
+//             await sendTestInfo(mobileNo, mode, testDate);
+//             //token generate
+//             // const token = generateToken(user._id, user.mobileNo);
+//             return res.status(201).json({ data: { user, isNew: false }, code: 200, status_code: "success", message: "User updated successfully." })
+//         } else {
+//             // If the center is full or not found, prevent user registration and return an error
+//             return res.status(400).json({ code: 400, status_code: 'error', message: 'Chosen test center is full please select other center' });
+//         }
+//     } catch (error) {
+//         console.log("error");
+//         res.status(500).json({ message: "something went wrong" });
+//     }
+
+// }
+
+
+// signup api for aset:
 
 const signup = async (req, res) => {
     //Existing user check
@@ -78,7 +202,7 @@ const signup = async (req, res) => {
         }
 
         // Check if the mode is "online" (no need to select a test center)
-        if (mode === 'online') {
+        if (mode === 'online' || mode === 'offline') {
             const user = await User.create({
                 mobileNo: mobileNo,
                 name: name,
@@ -108,35 +232,35 @@ const signup = async (req, res) => {
         // Check if the chosen test center has available capacity
         const center = await Center.findOne({ centerId: testCenterId });
 
-        if (center && center.capacity > 0) {
-            // //Hashed password
-            // const hashedPassword = await bcrypt.hash(password, 10);
-            //user create
-            const user = await User.create({
-                mobileNo: mobileNo,
-                name: name,
-                dob: dob,
-                fatherName: fatherName,
-                fatherMobileNo: fatherMobileNo,
-                stream: stream,
-                schoolName: schoolName,
-                city: city,
-                testDate: testDate,
-                mode: mode,
-                testCenter: center._id
+        // if (center && center.capacity > 0) {
+        //     // //Hashed password
+        //     // const hashedPassword = await bcrypt.hash(password, 10);
+        //     //user create
+        //     const user = await User.create({
+        //         mobileNo: mobileNo,
+        //         name: name,
+        //         dob: dob,
+        //         fatherName: fatherName,
+        //         fatherMobileNo: fatherMobileNo,
+        //         stream: stream,
+        //         schoolName: schoolName,
+        //         city: city,
+        //         testDate: testDate,
+        //         mode: mode,
+        //         testCenter: center._id
 
-            })
-            // Update the center's capacity
-            center.capacity -= 1;
-            await center.save();
-            await sendTestInfo(mobileNo, mode, testDate);
-            //token generate
-            // const token = generateToken(user._id, user.mobileNo);
-            return res.status(201).json({ data: { user, isNew: false }, code: 200, status_code: "success", message: "User updated successfully." })
-        } else {
-            // If the center is full or not found, prevent user registration and return an error
-            return res.status(400).json({ code: 400, status_code: 'error', message: 'Chosen test center is full please select other center' });
-        }
+        //     })
+        //     // Update the center's capacity
+        //     center.capacity -= 1;
+        //     await center.save();
+        //     await sendTestInfo(mobileNo, mode, testDate);
+        //     //token generate
+        //     // const token = generateToken(user._id, user.mobileNo);
+        //     return res.status(201).json({ data: { user, isNew: false }, code: 200, status_code: "success", message: "User updated successfully." })
+        // } else {
+        //     // If the center is full or not found, prevent user registration and return an error
+        //     return res.status(400).json({ code: 400, status_code: 'error', message: 'Chosen test center is full please select other center' });
+        // }
     } catch (error) {
         console.log("error");
         res.status(500).json({ message: "something went wrong" });
